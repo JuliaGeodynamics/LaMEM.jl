896f261ae35f4c3cf74067446ed2a2e033e439bf
Boris Kaus <kaus@uni-mainz.de>
2022-09-01 13:50:11 +0200

    added initial files

---
+++ src/LaMEM.jl
@@ -0,0 +1,19 @@
+module LaMEM
+
+using LaMEM_jll
+
+# load the correct mpi
+const mpiexec = if isdefined(LaMEM_jll,:MPICH_jll)
+    LaMEM_jll.MPICH_jll.mpiexec()
+elseif isdefined(LaMEM_jll,:MicrosoftMPI_jll) 
+    LaMEM_jll.MicrosoftMPI_jll.mpiexec()
+else
+    nothing
+end
+
+
+include("run_lamem.jl")
+
+
+
+end # module

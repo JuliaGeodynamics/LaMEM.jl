93bb34da0eb35355d24efc92f68e3aff45b6d151
Boris Kaus <kaus@uni-mainz.de>
2023-03-10 23:05:32 +0100

    remove python

--- src/LaMEM.jl
+++ src/LaMEM.jl
@@ -1,9 +1,6 @@
 module LaMEM
 using LaMEM_jll
-#using PythonCall
 using Glob
-
-#ENV["JULIA_CONDAPKG_BACKEND"] = "MicroMamba"
 
 # load the correct MPI
 const mpiexec = if isdefined(LaMEM_jll,:MPICH_jll)
@@ -14,21 +11,6 @@ else
     nothing
 end
 
-#=
-# Reading files back into julia
-const pyvtk = PythonCall.pynew()
-
-function __init__()
-
-    println("Adding PythonCall dependencies to read LaMEM timesteps")
-    pth = (@__DIR__)*"/python"        # Path where the python routines are
-    pyimport("sys").path.append(pth)  # append path
-  
-    # link vtk. Note that all python dependencies are listed in PythonCallDeps.toml
-    PythonCall.pycopy!(pyvtk, pyimport("vtk"))            # used to read VTK files
-end
-=#
-
 include("run_lamem.jl")
 include("run_lamem_save_grid.jl")
 include("read_timestep.jl")
@@ -36,6 +18,5 @@ include("utils.jl")
 
 export run_lamem
 export run_lamem_save_grid
-#export pyvtk
 
 end # module

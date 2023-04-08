69c7d8f54240a538adc667fc2a9a1403b0ffc50e
bkaus <kaus@uni-mainz.de>
2023-04-08 23:16:44 +0200

    cleanup

--- src/LaMEM.jl
+++ src/LaMEM.jl
@@ -12,27 +12,4 @@ using .Run
 export run_lamem, run_lamem_save_grid, mpiexec
 export remove_popup_messages_mac, show_paths_LaMEM
 
-
-
-#=
-using LaMEM_jll
-
-# load the correct MPI
-const mpiexec = if isdefined(LaMEM_jll,:MPICH_jll)
-    LaMEM_jll.MPICH_jll.mpiexec()
-elseif isdefined(LaMEM_jll,:MicrosoftMPI_jll) 
-    LaMEM_jll.MicrosoftMPI_jll.mpiexec()
-else
-    nothing
-end
-
-
-
-=#
-
-
-#include("read_timestep.jl")
-#include("utils.jl")
-
-
 end # module

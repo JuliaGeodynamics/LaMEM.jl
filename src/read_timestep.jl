d5b1bf66c092931e1955a8d83877eff608b57d74
bkaus <kaus@uni-mainz.de>
2023-04-10 10:44:23 +0200

    next attempt

--- src/read_timestep.jl
+++ src/read_timestep.jl
@@ -128,7 +128,9 @@ end
  # The FileName can contain a directory as well; deal with that here
 function split_path_name(DirName_base::String, FileName::String)
     FullName = joinpath(DirName_base,FileName)
-    id       = findlast(Base.Filesystem.path_separator, FullName)[1];
+    #id       = findlast(Base.Filesystem.path_separator, FullName)[1];
+    id       = findlast("/", FullName)[1];
+    
     DirName  = FullName[1:id-1]
     File     = FullName[id+1:end]
 
@@ -148,7 +150,9 @@ It will return `data_output` which is a `CartData` output structure.
 function Read_LaMEM_PVTR_File(DirName_base::String, FileName::String; fields=nothing)
     CurDir = pwd();
 
+    @show DirName_base FileName
     DirName, File = split_path_name(DirName_base, FileName)
+    @show DirName File
     
     # change to directory
     cd(DirName)
@@ -232,8 +236,8 @@ function readPVD(FileName::String)
         Time = push!(Time, time)
 
         # retrieve the timestep 
-        #file_name = split(file,Base.Filesystem.path_separator)[1];
-        file_name = split(file,"/")[1];
+        file_name = split(file,Base.Filesystem.path_separator)[1];
+        #file_name = split(file,"/")[1];
         
         timestep = parse(Int64,split(file_name,"_")[2]);
         Timestep = push!(Timestep, timestep)

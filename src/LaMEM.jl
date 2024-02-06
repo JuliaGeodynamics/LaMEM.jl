5cefce241c42195e805702ed09ff0b58de682b5c
Boris Kaus <kaus@uni-mainz.de>
2024-02-06 18:14:15 +0100

    add support for Phase Aggregates in julia

--- src/LaMEM.jl
+++ src/LaMEM.jl
@@ -30,9 +30,9 @@ export  LaMEM_Model, Model, Write_LaMEM_InputFile, create_initialsetup,
         Solver, ModelSetup, 
         geom_Sphere,
         Output,
-        Materials, Phase, Softening, PhaseTransition, Dike, PassiveTracers,
+        Materials, Phase, Softening, PhaseTransition, PhaseAggregate, Dike, PassiveTracers,
         add_vbox!, rm_vbox!, rm_last_vbox!, 
-        add_phase!, rm_phase!, rm_last_phase!, replace_phase!, add_petsc!, add_softening!,
+        add_phase!, rm_phase!, rm_last_phase!, replace_phase!, add_petsc!, add_softening!, add_phaseaggregate!,
         add_phasetransition!, add_dike!, add_geom!, set_air, copy_phase,
         add_topography!, AboveSurface!, BelowSurface!    
 

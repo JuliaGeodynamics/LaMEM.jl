887b1f02447fbf38e8536eaa1f607ac79fccf787
Boris Kaus <kaus@uni-mainz.de>
2024-02-07 18:54:20 +0100

    add "print_short" which prints a single string with MG options

--- src/LaMEM_ModelGeneration/Multigrid.jl
+++ src/LaMEM_ModelGeneration/Multigrid.jl
@@ -1,6 +1,6 @@
 # This comntains some helper functions to simplify setting up multigrid solvers for LaMEM
 
-export Multigrid
+export Multigrid, print_short
 
 """
     Structure that has info about setting up multigrid for LaMEM
@@ -137,8 +137,8 @@ function digitsep(value::Integer; seperator=",", per_separator=3)
     return (isnegative ? "-" : "") * join(groups, seperator)
 end
 
-function print_mg_level(nel::NTuple; pad=(4,4,4), pad_start="", pad_end="")
-    println("$(pad_start)# | $(lpad(nel[1],pad[1]))×$(lpad(nel[2],pad[2]))×$(lpad(nel[3],pad[3])) | $(pad_end)")
+function print_mg_level(io::IO, nel::NTuple; pad=(4,4,4), pad_start="", pad_end="")
+    println(io, "$(pad_start)# | $(lpad(nel[1],pad[1]))×$(lpad(nel[2],pad[2]))×$(lpad(nel[3],pad[3])) | $(pad_end)")
 end
 
 function max_pad(nel)
@@ -153,8 +153,8 @@ function show(io::IO, d::Multigrid)
     pad = max_pad(nel)
 
     dof = compute_dof(nel);   # compute degrees of freedom
-    println("# total dof: $(digitsep(dof,seperator="'"))")
-    println("# using $(d.cores) cores")
+    println(io,"# total dof: $(digitsep(dof,seperator="'"))")
+    println(io,"# using $(d.cores) cores")
     
     #
     println(io,"# --- $(d.levels) MG levels ---- ")
@@ -164,97 +164,125 @@ function show(io::IO, d::Multigrid)
         else
             pad_end=" - $(d.cores) cores"
         end
-        print_mg_level(nel, pad=pad, pad_end=pad_end)
+        print_mg_level(io, nel, pad=pad, pad_end=pad_end)
 
         if all(mod.(nel,2) .== 0) && l<d.levels
             nel = nel./2
             nel = Int64.(nel)
         elseif all(mod.(nel,2) .!= 0)
-            println("Cannot create multigrid for level $(l+1)")
+            println(io,"Cannot create multigrid for level $(l+1)")
         end
     end
-    println("-gmg_pc_mg_levels $(d.levels)")
+    println(io,"-gmg_pc_mg_levels $(d.levels)")
 
     # Setup main multigrid
-    println("-gmg_pc_type mg")
-    println("-gmg_pc_mg_galerkin")
-    println("-gmg_pc_mg_type multiplicative")
-    println("-gmg_pc_mg_cycle_type v")
+    println(io,"-gmg_pc_type mg")
+    println(io,"-gmg_pc_mg_galerkin")
+    println(io,"-gmg_pc_mg_type multiplicative")
+    println(io,"-gmg_pc_mg_cycle_type v")
     
     # Setup smoothener per level
     if d.smoother=="jacobi"
-        println("-gmg_mg_levels_ksp_type richardson")
-        println("-gmg_mg_levels_ksp_richardson_scale 0.5")
-        println("-gmg_mg_levels_pc_type jacobi")
+        println(io,"-gmg_mg_levels_ksp_type richardson")
+        println(io,"-gmg_mg_levels_ksp_richardson_scale 0.5")
+        println(io,"-gmg_mg_levels_pc_type jacobi")
 	elseif d.smoother=="chebyshev"
-        println("-gmg_mg_levels_ksp_type chebyshev")
+        println(io,"-gmg_mg_levels_ksp_type chebyshev")
     else
-        println("-gmg_mg_levels_pc_type $(d.smoother)")
+        println(io,"-gmg_mg_levels_pc_type $(d.smoother)")
     end
-    println("-gmg_mg_levels_ksp_max_it $(d.smooth)")
-    println(" ")
+    println(io,"-gmg_mg_levels_ksp_max_it $(d.smooth)")
+    println(io," ")
 
     min_cores_coarse = prod(nel)
     if d.cores>min_cores_coarse
-        println("Warning: Coarse grid solver can only be done on $(min_cores_coarse) cores")
+        println(io,"Warning: Coarse grid solver can only be done on $(min_cores_coarse) cores")
     end
 
-    println("    # ---- Coarse Grid: $( d.coarse_pc) ")
+    println(io,"    # ---- Coarse Grid: $( d.coarse_pc) ")
 
     num_cores_coarse = d.cores_coarse
     if  d.coarse_pc=="superlu_dist" ||  d.coarse_pc=="mumps"
         num_cores_coarse = d.cores
     end
 
-    print_mg_level(nel, pad=pad, pad_start="    ", pad_end = " - $num_cores_coarse cores")
+    print_mg_level(io, nel, pad=pad, pad_start="    ", pad_end = " - $num_cores_coarse cores")
     reduction_factor  = Int64(d.cores/d.cores_coarse)
     if d.coarse_pc=="telescope"
-        println("    -crs_ksp_type $(d.coarse_ksp)")
-        println("    -crs_pc_type telescope")
-        println("    -crs_pc_telescope_reduction_factor $(Int64(reduction_factor))")
+        println(io,"    -crs_ksp_type $(d.coarse_ksp)")
+        println(io,"    -crs_pc_type telescope")
+        println(io,"    -crs_pc_telescope_reduction_factor $(Int64(reduction_factor))")
 
         if (d.coarse_coarse_pc=="superlu_dist") || (d.coarse_coarse_pc=="mumps")
-            println("    -crs_telescope_ksp_type preonly")
-            println("    -crs_telescope_pc_type lu")
-            println("    -crs_telescope_pc_factor_mat_solver_type $(d.coarse_coarse_pc)")
+            println(io,"    -crs_telescope_ksp_type preonly")
+            println(io,"    -crs_telescope_pc_type lu")
+            println(io,"    -crs_telescope_pc_factor_mat_solver_type $(d.coarse_coarse_pc)")
 
         elseif (d.coarse_coarse_pc=="gamg") 
-                println("    -crs_telescope_ksp_type $(d.coarse_coarse_ksp)")
-                println("    -crs_telescope_pc_type gamg")
-                println("        # ---- GAMG coarse grid solver")
-                println("        -crs_telescope_pc_gamg_coarse_eq_limit $(d.gamg_coarse_eq_limit)")
-                println("        -crs_telescope_pc_gamg_threshold $(d.gamg_threshold)")
-                println("        -crs_telescope_pc_gamg_repartition $(Int64(d.gamg_repartition))")
-                println("        -crs_telescope_pc_gamg_use_parallel_coarse_grid_solver $(Int64(d.gamg_parallel_coarse))")
+                println(io,"    -crs_telescope_ksp_type $(d.coarse_coarse_ksp)")
+                println(io,"    -crs_telescope_pc_type gamg")
+                println(io,"        # ---- GAMG coarse grid solver")
+                println(io,"        -crs_telescope_pc_gamg_coarse_eq_limit $(d.gamg_coarse_eq_limit)")
+                println(io,"        -crs_telescope_pc_gamg_threshold $(d.gamg_threshold)")
+                println(io,"        -crs_telescope_pc_gamg_repartition $(Int64(d.gamg_repartition))")
+                println(io,"        -crs_telescope_pc_gamg_use_parallel_coarse_grid_solver $(Int64(d.gamg_parallel_coarse))")
             
         else
-            println("    -crs_telescope_ksp_type $(d.coarse_coarse_ksp)")
-            println("    -crs_telescope_pc_type $(d.coarse_coarse_pc)")
+            println(io,"    -crs_telescope_ksp_type $(d.coarse_coarse_ksp)")
+            println(io,"    -crs_telescope_pc_type $(d.coarse_coarse_pc)")
         end
 
     elseif d.coarse_pc=="superlu_dist"
-        println("    -crs_ksp_type $(d.coarse_ksp)")
-        println("    -crs_pc_type lu")
-        println("    -crs_pc_factor_mat_solver_type $(d.coarse_pc)")
+        println(io,"    -crs_ksp_type $(d.coarse_ksp)")
+        println(io,"    -crs_pc_type lu")
+        println(io,"    -crs_pc_factor_mat_solver_type $(d.coarse_pc)")
 
     elseif d.coarse_pc=="redundant"
       
         # use a redundant solver
-        println("    -crs_ksp_type $(d.coarse_ksp)")
-        println("    -crs_pc_type redundant")
-        println("    -crs_pc_redundant_number $reduction_factor")
-        println("    -crs_pc_redundant_ksp_type $(d.coarse_coarse_ksp)")
+        println(io,"    -crs_ksp_type $(d.coarse_ksp)")
+        println(io,"    -crs_pc_type redundant")
+        println(io,"    -crs_pc_redundant_number $reduction_factor")
+        println(io,"    -crs_pc_redundant_ksp_type $(d.coarse_coarse_ksp)")
 
         if (d.coarse_coarse_pc=="superlu_dist") || (d.coarse_coarse_pc=="mumps")
-            println("    -crs_pc_redundant_pc_type lu")
-            println("    -crs_redundant_pc_factor_mat_solver_type $(d.coarse_coarse_pc)")
+            println(io,"    -crs_pc_redundant_pc_type lu")
+            println(io,"    -crs_redundant_pc_factor_mat_solver_type $(d.coarse_coarse_pc)")
         else
-            println("    -crs_pc_redundant_pc_type $(d.coarse_coarse_pc)")
+            println(io,"    -crs_pc_redundant_pc_type $(d.coarse_coarse_pc)")
         end
     else
-        println("    -crs_pc_type $(d.coarse_pc)")
+        println(io,"    -crs_pc_type $(d.coarse_pc)")
     end
 
     
     return nothing
+end
+
+
+"""
+This creates a single string, so we can use it in the command line
+"""
+function print_short(d::Multigrid)
+
+    # get string with output
+    io = IOBuffer();
+    show(IOContext(io, :limit => true, :displaysize => (10, 10)), "text/plain", d);
+    s = String(take!(io));
+
+    # vector with lines
+    s_vec = split(s,"\n")
+
+    # remove comments
+    s_vec = strip.(s_vec)
+    s_vec = s_vec[startswith.(s_vec,"#") .== false]
+    s_vec = s_vec[length.(s_vec).>0]    # remove empty lines
+
+    # create single string
+    str = " ";
+    for s in s_vec
+        str *= s * " ";
+    end
+
+    return str
 end

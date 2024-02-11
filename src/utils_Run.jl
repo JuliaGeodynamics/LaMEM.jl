7541abda7e0bf5a88e304bac94f78238035a2fee
Boris Kaus <kaus@uni-mainz.de>
2024-02-11 12:13:44 +0100

    add formatted output

--- src/utils_Run.jl
+++ src/utils_Run.jl
@@ -78,7 +78,7 @@ end
 This reads a LaMEM logfile (provided it was run with "-log_view") and collects key results from it; 
 mostly for scalability tests on HPC machines. It returns a markdown summary
 """
-function read_LaMEM_logfile(Filename::String; ID=nothing)
+function read_LaMEM_logfile(Filename::String; ID=nothing, header=true)
     
     # Read file as vector of strings
     f = open(Filename)
@@ -86,32 +86,48 @@ function read_LaMEM_logfile(Filename::String; ID=nothing)
     close(f)
 
     # Extract information from logfile
-    Cores       = extract_info_logfile(lines, "Total number of cpu                  :")
+    Cores       = Int64(extract_info_logfile(lines, "Total number of cpu                  :",LaMEM=false, entry=1))
     FineGrid    = extract_info_logfile(lines, "Fine grid cells [nx, ny, nz]         :")
     CoarseGrid  = extract_info_logfile(lines, "Global coarse grid [nx,ny,nz] :")
     Levels      = parse(Int64,extract_info_logfile(lines, "Number of multigrid levels    :"))
 
-    TotalTime_s  = extract_info_logfile(lines, "Time (sec):", LaMEM=false)
-    CoarseTime_s = extract_info_logfile(lines, "MGSmooth Level 0", LaMEM=false, entry=3)
-    Iter         = Int64(extract_info_logfile(lines, "SNESSolve", LaMEM=false, entry=1))
-
-
+    TotalTime    = extract_info_logfile(lines, "Time (sec):", LaMEM=false)
+    CoarseTime   = extract_info_logfile(lines, "MGSmooth Level 0", LaMEM=false, entry=3)
+    SNES         = Int64(extract_info_logfile(lines, "SNESSolve", LaMEM=false, entry=1))
+    KSP          = Int64(extract_info_logfile(lines, "KSPSolve", LaMEM=false, entry=1))
+    RedFactor    = Int64(extract_info_logfile(lines, "-crs_pc_telescope_reduction_factor", LaMEM=false, entry=1))
+    CoaCores     = Int64(Cores/RedFactor)
+ 
     # Retrieve memory usage if we have system with slurm (use seff)
     if isnothing(ID)
         ID = split(split(Filename,".")[1],"_")[end]
+        ID = parse(Int64,ID)
     end
-    lines_mem = execute_command("seff $ID"); # run code
+    lines_mem = execute_command(ID); # run code
     if !isnothing(lines_mem)
-        Memory  = extract_info_logfile(lines, "Memory Utilized:", LaMEM=false, entry=1)
+        Memory_Gb  = extract_info_logfile(lines_mem, "Memory Utilized:", LaMEM=false, entry=1)
+        #Memory_Gb = extract_info_logfile(lines_mem, "Memory Utilized:", LaMEM=false, entry=5)
+
+        Nodes      = Int64(extract_info_logfile(lines_mem, "Nodes:", LaMEM=false, entry=1))
     else
-        Memory = "-"
+        Memory_Gb = "-"
+        Nodes = "-"
     end
+    MemoryNode_Gb = Memory_Gb/Nodes
 
     # print as Markdown table
-    table = (; FineGrid, Cores, CoarseGrid, Levels, Iter, TotalTime_s, CoarseTime_s, Memory, Filename) 
-    println(markdown_table([table], String))
+    table = (; FineGrid, Cores, Nodes, CoarseGrid, CoaCores, Levels, SNES, KSP, TotalTime, CoarseTime, MemoryNode_Gb, Filename) 
+    print_table_markdown(table, header=header)
 
     return lines
+end
+
+
+function read_LaMEM_logfile(Filename::Vector{String}; ID=nothing)
+    read_LaMEM_logfile(Filename[1]; ID=ID, header=true)
+    for i = 2: length(Filename) 
+        read_LaMEM_logfile(Filename[i]; ID=ID, header=false)
+    end
 end
 
 add_str(str, keyword, pad) = str*"| $(rpad(keyword,pad))"
@@ -137,7 +153,7 @@ julia> coarse_grid_solve = extract_info_logfile(lines, "MGSmooth Level 0", LaMEM
 """
 function extract_info_logfile(lines, keyword::String; entry=1, LaMEM=true)
     # find the line with the keyword
-    idx = findfirst(x->occursin(keyword,x), lines)
+    idx = findlast(x->occursin(keyword,x), lines)
     if idx==nothing
         return "-"
     end
@@ -163,11 +179,11 @@ end
 
 Executes a command-line code and returns the output as a string
 """
-function execute_command(command="ls")
+function execute_command(ID=104810)
     
     # execute command 
     io = IOBuffer();
-    cmd = pipeline(`$command`; stdout=io, stderr=devnull);
+    cmd = pipeline(`seff $ID`; stdout=io, stderr=devnull);
 
     str = nothing
     try 
@@ -181,6 +197,32 @@ function execute_command(command="ls")
     return str
 end
 
+function print_table_markdown(table; header=true)
+    nam = String.(keys(table))
+    val = values(table)
+    le = max.(length.(nam),length.(val))     # length of each fieldnames
+    sep = repeat.("-",le);
 
+    if header
+        max_n = 3;
+        names = nam;
+    else
+        max_n = 1;
+        names = val
+    end
+    str = "";
+    for i=1:max_n
+        str = str*"| ";
+        if i==2;        names = sep
+        elseif i==3;    names = val
+        end
+        
+        for (j,na) in enumerate(names)
+            str *= "$(rpad(na,le[j])) | "
+        end
+        str *= "\n";
+    end
 
-
+    print(str)
+    return nothing
+end

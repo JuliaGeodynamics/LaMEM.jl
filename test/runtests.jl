a5189842c08c0f6da3fd6faa73bbd4fa916aa9f6
Boris Kaus <kaus@uni-mainz.de>
2024-02-17 22:39:12 +0100

    activate all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,12 @@
 using Test
 using LaMEM
 
-#include("test_julia_setups.jl")
-#include("runLaMEM.jl")
-#include("read_timestep.jl")
-#include("run_lamem_save_grid_test.jl")
-#include("mesh_refinement_test.jl")
-#include("read_logfile.jl")
+include("test_julia_setups.jl")
+include("runLaMEM.jl")
+include("read_timestep.jl")
+include("run_lamem_save_grid_test.jl")
+include("mesh_refinement_test.jl")
+include("read_logfile.jl")
 include("test_compression.jl")
 
 if !Sys.iswindows()

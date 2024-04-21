9804f8d3b7d8b85fe1729650a879e752d9dffe61
Boris Kaus <kaus@uni-mainz.de>
2024-04-21 21:08:59 +0200

    update tests (only test examples for now)

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -3,27 +3,19 @@ using Test
 
 @testset "examples in /scripts" begin
 
-    testing = true
 
     # Subduction example
     @testset "TM_Subduction_example" begin
+        testing = true
         include("../scripts/TM_Subduction_example.jl")
         data,time = read_LaMEM_timestep(model,last=true);
         @test time ≈ 0.004419778
         @test sum(data.fields.velocity[3][:,:,:]) ≈ 708.41907f0 rtol=1e-4 # check Vz
     end
 
-    # FB example
-    @testset "FB_restart" begin
-        clean_directory()
-        include("../scripts/FB_restart.jl")
-        data,time = read_LaMEM_timestep(model,last=true);
-        @test time ≈ 63.0025
-        @test sum(data.fields.velocity[3][:,:,:]) ≈ 0.07041338f0 rtol=1e-4 # check Vz
-    end
-
     # 3D subduction example
     @testset "Subduction3D" begin
+        testing = true
         clean_directory()
         include("../scripts/Subduction3D.jl")
         data,time = read_LaMEM_timestep(model,last=true);
@@ -33,6 +25,7 @@ using Test
 
     # Strength envelop example
     @testset "StrengthEnvelop" begin
+        testing = true
         clean_directory()
         include("../scripts/StrengthEnvelop.jl")
         data,time = read_LaMEM_timestep(model,last=true);

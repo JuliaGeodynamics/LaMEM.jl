634130ec3db7e7f3ea3eca0edca1fda3be758302
Boris Kaus <kaus@uni-mainz.de>
2024-05-23 12:41:13 +0200

    deactivate some tests for windows

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -6,21 +6,26 @@ const testing = true
 
 
     # Subduction example
-    @testset "TM_Subduction_example" begin
-        clean_directory()
-        include("../scripts/TM_Subduction_example.jl")
-        data,time = read_LaMEM_timestep(model,last=true);
-        @test time ≈ 0.0021
-        @test sum(data.fields.velocity[3][:,:,:]) ≈ 420.10352f0 rtol=1e-4 # check Vz
+    if !iswindows()
+        @testset "TM_Subduction_example" begin
+            clean_directory()
+            include("../scripts/TM_Subduction_example.jl")
+            data,time = read_LaMEM_timestep(model,last=true);
+            @test time ≈ 0.0021
+            @test sum(data.fields.velocity[3][:,:,:]) ≈ 420.10352f0 rtol=1e-4 # check Vz
+        end
+    
     end
 
     # 3D subduction example
-    @testset "Subduction3D" begin
-        clean_directory()
-        include("../scripts/Subduction3D.jl")
-        data,time = read_LaMEM_timestep(model,last=true);
-        @test time ≈ 0.05504613
-        @test sum(data.fields.velocity[3][:,:,:]) ≈ -51.314083f0 rtol=1e-4 # check Vz
+    if !iswindows()
+        @testset "Subduction3D" begin
+            clean_directory()
+            include("../scripts/Subduction3D.jl")
+            data,time = read_LaMEM_timestep(model,last=true);
+            @test time ≈ 0.05504613
+            @test sum(data.fields.velocity[3][:,:,:]) ≈ -51.314083f0 rtol=1e-4 # check Vz
+        end
     end
 
     # Strength envelop example

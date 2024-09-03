f37dc57dbef0a8bf67c4ee9082ce24189a29f265
Wenrong Cao <caowenrong@gmail.com>
2024-09-02 21:46:49 -0700

    changed paths in test_examples.jl and add PassiveTracers test

---
+++ example_scripts/PassiveTracers.jl
@@ -0,0 +1,14 @@
+# This is .jl version of the PassiveTracers example. 
+# We use this .jl file to run the test but you can also run the code independently.
+# The ipynb file of this code with additional notes for initiation and plotting tracers is located in the same directory: "PassiveTracers.ipynb".
+
+using LaMEM, GeophysicalModelGenerator, Plots
+
+model  = Model(Grid(nel=(16,16,16), x=[-1,1], y=[-1,1], z=[-1,1]), PassiveTracers(Passive_Tracer=1, PassiveTracer_Box=[-1,1,-1,1,-1,1]))
+matrix = Phase(ID=0,Name="matrix",eta=1e20,rho=3000)
+sphere = Phase(ID=1,Name="sphere",eta=1e23,rho=3200)
+add_phase!(model, sphere, matrix)
+add_sphere!(model,cen=(0.0,0.0,0.0), radius=(0.5,))
+
+run_lamem(model,1)
+
--- test/test_examples.jl
+++ test/test_examples.jl
@@ -12,7 +12,7 @@ const testing = true
     if !Sys.iswindows()
         @testset "Subduction3D" begin
             clean_directory()
-            include("../scripts/Subduction3D.jl")
+            include("../example_scripts/Subduction3D.jl")
             data,time = read_LaMEM_timestep(model,last=true);
             @test time ≈ 0.05504613
             @test sum(data.fields.velocity[3][:,:,:]) ≈ -51.314083f0 rtol=1e-4 # check Vz
@@ -22,21 +22,32 @@ const testing = true
     # Strength envelop example
     @testset "StrengthEnvelop" begin
         clean_directory()
-        include("../scripts/StrengthEnvelop.jl")
+        include("../example_scripts/StrengthEnvelop.jl")
         data,time = read_LaMEM_timestep(model,last=true);
         @test time ≈ 0.09834706
         @test sum(data.fields.velocity[3][:,:,:]) ≈ 14.305277f0 rtol=1e-4 # check Vz
     end
 
-
     # Subduction example
     if !Sys.iswindows()
         @testset "TM_Subduction_example" begin
             clean_directory()
-            include("../scripts/TM_Subduction_example.jl")
+            include("../example_scripts/TM_Subduction_example.jl")
             data,time = read_LaMEM_timestep(model,last=true);
             @test time ≈ 0.0021
             @test sum(data.fields.velocity[3][:,:,:]) ≈ 420.10352f0 rtol=1e-4 # check Vz
+        end
+    
+    end
+
+    # PassiveTracers example
+    if !Sys.iswindows()
+        @testset "PassiveTracers" begin
+            clean_directory()
+            include("../example_scripts/PassiveTracers.jl")
+            data,time = read_LaMEM_timestep(model,last=true);
+            @test time ≈ 1.078999
+            @test sum(data.fields.velocity[3][:,:,:]) ≈ 0.1642956f0 rtol=1e-4 # check Vz
         end
     
     end

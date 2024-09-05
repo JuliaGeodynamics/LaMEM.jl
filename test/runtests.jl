36aa0efda2dc650d8625a3a95f918bc3607715f0
Boris Kaus <kaus@uni-mainz.de>
2024-09-05 08:39:00 +0200

    add overview of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,19 @@
 using Test
 using LaMEM
 
-include("test_julia_setups.jl")
-include("runLaMEM.jl")
-include("read_timestep.jl")
-include("run_lamem_save_grid_test.jl")
-include("mesh_refinement_test.jl")
-include("read_logfile.jl")
-include("test_compression.jl")
-include("test_GeoParams_integration.jl")
-include("test_examples.jl")
+@testset verbose = true "LaMEM.jl" begin
+
+    include("test_julia_setups.jl")
+    include("runLaMEM.jl")
+    include("read_timestep.jl")
+    include("run_lamem_save_grid_test.jl")
+    include("mesh_refinement_test.jl")
+    include("read_logfile.jl")
+    include("test_compression.jl")
+    include("test_GeoParams_integration.jl")
+    include("test_examples.jl")
+
+end
 
 if !Sys.iswindows()
     # clean up
